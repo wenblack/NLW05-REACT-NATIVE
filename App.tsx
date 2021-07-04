1c2f202c8c579b1430eb9a3689b4e838623a2c79
wenblack <wenblack012@gmail.com>
2021-07-03 23:42:37 -0300

    Animação SplashScreen e Alteração da fonte
    Criado animação para deixar Splash Screen rodando enquanto o app não inicia. Alterado fonte para uma do Google.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 
 //Importando bibliotecas necessárias
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+
+} from '@expo-google-fonts/jost';
 import { Welcome } from './src/assets/Telas/boasvindas';
-//Criando primeiro componente/função
+
+
+
+//Criando componente principal
 export default function App() {
+
+  const [carregandoFonte] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if (!carregandoFonte)
+
+    return <AppLoading />
+
   return (
+
     <Welcome />
+
 
   );
 }
--- src/assets/Telas/boasvindas.tsx
+++ src/assets/Telas/boasvindas.tsx
@@ -1,25 +1,34 @@
 
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+    SafeAreaView,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    Dimensions
+}
+    from 'react-native';
 import wateringImg from '../../../src/assets/watering.png';
 import colors from '../../../styles/colors';
+import { Feather } from '@expo/vector-icons';
 
 
-/*Exemplo de Interface:
+/*Exemplo de uso de duas interfaces em um mesmo componente:
 ***************************************************************************
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacity {
     title: string;
 }
 *******************************************************************************
 Implementando na funcção:
-export function Button({ title }: ButtonProps) {
+export function Button({ title, ... rest }: ButtonProps) {
     <Text>
         {title}
     </Text>
 *********************************************************************************************
 Usando componente
 
-<Button title={"Teste de interface"}></Button>
+<Button title={"Teste de interface" onPress={}}></Button>
 
 */
 export function Welcome() {
@@ -32,14 +41,25 @@ export function Welcome() {
                 suas plantas {'\n'}
                 de forma fácil</Text>
 
-            <Image source={wateringImg} style={style.imagem}></Image>
-            <Text style={style.subtitulo}>Não esqueça mais de regar suas plantas.
+            <Image
+                source={wateringImg}
+                style={style.imagem}
+                resizeMode='contain'
+            >
+            </Image>
+            <Text style={style.subtitulo}>
+                Não esqueça mais de regar suas plantas.
                 Nós cuidamos de lembrar você
                 sempre que precisar.</Text>
             <TouchableOpacity style={style.botao}
                 activeOpacity={0.7}
             >
-                <Text style={style.textoBotao}>></Text>
+                <Text style={style.textoBotao}>
+                    <Feather
+                        name='chevron-right'
+                        style={style.icone}
+                    ></Feather>
+                </Text>
             </TouchableOpacity>
 
 
@@ -47,10 +67,12 @@ export function Welcome() {
         </SafeAreaView>
     );
 }
-//Criando primeiro estilo
+//Estilos
 const style = StyleSheet.create({
     container: {
-        flex: 1, justifyContent: 'space-evenly', alignItems: 'center'
+        flex: 1,
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
     },
     titulo: {
         fontSize: 32,
@@ -66,24 +88,33 @@ const style = StyleSheet.create({
         paddingHorizontal: 20,
         color: colors.heading
     },
+
+    imagem: {
+        height: 284,
+        width: 292
+    },
     botao: {
         backgroundColor: colors.green,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 16,
         marginBottom: 10,
-        height: 56,
-        width: 56
+        height: 55,
+        width: 55
 
     },
-    imagem: {
-        width: 292,
-        height: 284
-    },
+
     textoBotao: {
         color: colors.white,
-        fontSize: 25
+        fontSize: 25,
+
+    },
+    icone: {
+        fontSize: 31,
+        color: colors.white,
+        alignSelf: 'center',
     }
+
 
 })
 
